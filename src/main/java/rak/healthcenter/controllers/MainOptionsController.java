33eb78ca1dfb6a80fe3d2730fc2c21435d87a31d
iceburg333 <ice_burg333@yahoo.com>
2017-08-13 12:48:33 -0400

    Main Options Screen is functional but does not call outside code

--- src/main/java/rak/healthcenter/controllers/MainOptionsController.java
+++ src/main/java/rak/healthcenter/controllers/MainOptionsController.java
@@ -1,5 +1,7 @@
 package rak.healthcenter.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -14,19 +16,58 @@ public class MainOptionsController {
 	@FXML private Button advanceAge;
 	@FXML private Button addRandomConditions;
 	
+	private static final int AGE_AMOUNT_DEFAULT = 1;
+	private static final int CONDITION_COUNT_DEFAULT = 3;
+	
+	
+	public void initialize(){
+		initializeAgeControls();
+		initializeRandomConditionControls();
+	}
+
+	public void initializeAgeControls() {
+		ageAmount.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				advanceAge.setText("Advance Age by " + parseInt(newValue, AGE_AMOUNT_DEFAULT));
+			}
+		});
+		ageAmount.setText("" + AGE_AMOUNT_DEFAULT);
+	}
+
+	public void initializeRandomConditionControls() {
+		conditionCount.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				addRandomConditions.setText("Add " + parseInt(newValue, CONDITION_COUNT_DEFAULT) + " Random Conditions");
+			}
+		});
+		conditionCount.setText("" + CONDITION_COUNT_DEFAULT);
+	}
+	
 	public void setHealthStationHelper(HealthStationHelper healthStationHelper){
 		this.healthStationHelper = healthStationHelper;
 	}
-
 	
-	@FXML
-	public void addRandomConditions(){
-		
+	private int parseInt(String in, int defaultValue){
+		try {
+			return Integer.parseInt(in);
+		} catch(Exception e){
+			System.out.println(in + " must be an int");
+		}
+		return defaultValue;
 	}
 	
 	@FXML
 	public void advanceAge(){
-		
+		System.out.println("Advance time by " + parseInt(ageAmount.getText(), AGE_AMOUNT_DEFAULT));
 	}
+
+	@FXML
+	public void addRandomConditions(){
+		System.out.println("Add " + parseInt(conditionCount.getText(), CONDITION_COUNT_DEFAULT) + " random condtions");
+	}
+	
+
 	
 }
